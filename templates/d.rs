edd6b02bf262063b12a3911a41f3fa646c0f16bb
mwcz <mwc@clayto.com>
2023-11-29 21:23:15 -0500

    disable tests by default in template

--- templates/d.rs
+++ templates/d.rs
@@ -16,42 +16,42 @@ pub fn part2(input: Model) -> Answer {
     "incomplete".to_string()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const INPUT: &str = include_str!("../input/d$AOC_DAY");
-    const EXAMPLE: &str = include_str!("../examples/d$AOC_DAY");
-
-    #[test]
-    fn d${AOC_DAY}p1_example_test() {
-        assert_eq!(
-            part1(parse(EXAMPLE.to_string())),
-            "put part 1 example answer here"
-        );
-    }
-
-    #[test]
-    fn d${AOC_DAY}p1_input_test() {
-        assert_eq!(
-            part1(parse(INPUT.to_string())), 
-            "put part 1 final answer here"
-        );
-    }
-
-    #[test]
-    fn d${AOC_DAY}p2_example_test() {
-        assert_eq!(
-            part2(parse(EXAMPLE.to_string())),
-            "put part 2 example answer here"
-        );
-    }
-
-    #[test]
-    fn d${AOC_DAY}p2_input_test() {
-        assert_eq!(
-            part2(parse(INPUT.to_string())),
-            "put part 2 final answer here"
-        );
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     const INPUT: &str = include_str!("../input/d$AOC_DAY");
+//     const EXAMPLE: &str = include_str!("../examples/d$AOC_DAY");
+//
+//     // #[test]
+//     // fn d${AOC_DAY}p1_example_test() {
+//     //     assert_eq!(
+//     //         part1(parse(EXAMPLE.to_string())),
+//     //         "put part 1 example answer here"
+//     //     );
+//     // }
+//     //
+//     // #[test]
+//     // fn d${AOC_DAY}p1_input_test() {
+//     //     assert_eq!(
+//     //         part1(parse(INPUT.to_string())),
+//     //         "put part 1 final answer here"
+//     //     );
+//     // }
+//     //
+//     // #[test]
+//     // fn d${AOC_DAY}p2_example_test() {
+//     //     assert_eq!(
+//     //         part2(parse(EXAMPLE.to_string())),
+//     //         "put part 2 example answer here"
+//     //     );
+//     // }
+//     //
+//     // #[test]
+//     // fn d${AOC_DAY}p2_input_test() {
+//     //     assert_eq!(
+//     //         part2(parse(INPUT.to_string())),
+//     //         "put part 2 final answer here"
+//     //     );
+//     // }
+// }
