0ddc7422943410a76757d0b098d15b33e0aa289e
mwcz <mwc@clayto.com>
2023-12-04 10:37:15 -0500

    day 4

--- src/d4.rs
+++ src/d4.rs
@@ -1,57 +1,110 @@
 //! A solution to day 4 year 2023.
 //! https://adventofcode.com/2023/day/4
 
-type Model = u8;
-type Answer = String;
+type Model = Vec<(Vec<u32>, Vec<u32>)>;
+type Answer = u32;
 
 pub fn parse(input: String) -> Model {
-    0
+    input
+        .lines()
+        .map(|line| {
+            let (name, nums) = line.split_once(':').unwrap();
+            let (wins, haves) = nums.split_once('|').unwrap();
+            let wins = wins
+                .split_whitespace()
+                .map(|n| n.trim().parse().unwrap())
+                .collect();
+            let haves = haves
+                .split_whitespace()
+                .map(|n| n.trim().parse().unwrap())
+                .collect();
+            (wins, haves)
+        })
+        .collect()
 }
 
-pub fn part1(model: Model) -> Answer {
-    "incomplete".to_string()
+pub fn part1(cards: Model) -> Answer {
+    let mut total_score = 0;
+    for (wins, haves) in cards {
+        let mut card_score = 0;
+        for n in haves {
+            if wins.contains(&n) {
+                card_score += 1;
+            }
+        }
+        if card_score > 0 {
+            total_score += 2u32.pow(card_score - 1);
+        }
+    }
+
+    total_score
 }
 
-pub fn part2(model: Model) -> Answer {
-    "incomplete".to_string()
+pub fn part2(mut cards: Model) -> Answer {
+    #[derive(Clone)]
+    struct Card {
+        wins: Vec<u32>,
+        haves: Vec<u32>,
+        matches: usize,
+        copies: u32,
+    }
+
+    let mut cards: Vec<Card> = cards
+        .into_iter()
+        .map(|(wins, haves)| Card {
+            matches: haves.iter().filter(|h| wins.contains(h)).count(),
+            wins,
+            haves,
+            copies: 1,
+        })
+        .collect();
+
+    let mut i = 0;
+    loop {
+        let matches = cards[i].matches;
+        let copies = cards[i].copies;
+
+        for _ in 0..copies {
+            for j in 1..=matches {
+                if let Some(c) = cards.get_mut(i + j) {
+                    c.copies += 1;
+                }
+            }
+        }
+
+        i += 1;
+        if i == cards.len() {
+            break;
+        }
+    }
+
+    cards.iter().map(|c| c.copies).sum()
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     const INPUT: &str = include_str!("../input/d4");
-//     const EXAMPLE: &str = include_str!("../examples/d4");
-//
-//     // #[test]
-//     // fn d4p1_example_test() {
-//     //     assert_eq!(
-//     //         part1(parse(EXAMPLE.to_string())),
-//     //         "put part 1 example answer here"
-//     //     );
-//     // }
-//     //
-//     // #[test]
-//     // fn d4p1_input_test() {
-//     //     assert_eq!(
-//     //         part1(parse(INPUT.to_string())),
-//     //         "put part 1 final answer here"
-//     //     );
-//     // }
-//     //
-//     // #[test]
-//     // fn d4p2_example_test() {
-//     //     assert_eq!(
-//     //         part2(parse(EXAMPLE.to_string())),
-//     //         "put part 2 example answer here"
-//     //     );
-//     // }
-//     //
-//     // #[test]
-//     // fn d4p2_input_test() {
-//     //     assert_eq!(
-//     //         part2(parse(INPUT.to_string())),
-//     //         "put part 2 final answer here"
-//     //     );
-//     // }
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = include_str!("../input/d4");
+    const EXAMPLE: &str = include_str!("../examples/d4");
+
+    #[test]
+    fn d4p1_example_test() {
+        assert_eq!(part1(parse(EXAMPLE.to_string())), 13);
+    }
+
+    #[test]
+    fn d4p1_input_test() {
+        assert_eq!(part1(parse(INPUT.to_string())), 33950);
+    }
+
+    #[test]
+    fn d4p2_example_test() {
+        assert_eq!(part2(parse(EXAMPLE.to_string())), 30);
+    }
+
+    #[test]
+    fn d4p2_input_test() {
+        assert_eq!(part2(parse(INPUT.to_string())), 14814534);
+    }
+}
