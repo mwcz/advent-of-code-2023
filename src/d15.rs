497817e3f175d33de38922f14adecaed02e53884
mwcz <mwc@clayto.com>
2023-12-20 22:42:08 -0500

    day 15

--- src/d15.rs
+++ src/d15.rs
@@ -1,57 +1,113 @@
 //! A solution to day 15 year 2023.
 //! https://adventofcode.com/2023/day/15
 
-type Model = u8;
-type Answer = String;
+type Model = Vec<String>;
+type Answer = usize;
 
 pub fn parse(input: String) -> Model {
-    0
+    input
+        .trim()
+        .split(',')
+        .map(|step| step.to_string())
+        .collect()
 }
 
 pub fn part1(model: Model) -> Answer {
-    "incomplete".to_string()
+    model.into_iter().map(hash).sum()
 }
 
 pub fn part2(model: Model) -> Answer {
-    "incomplete".to_string()
+    let mut boxes = vec![Box::default(); 256];
+    for step in model {
+        let (label, focal_length) = step.split_at(step.find(['-', '=']).unwrap());
+        let idx = hash(label);
+        if let Some(existing_lens) = boxes[idx]
+            .lenses
+            .iter()
+            .position(|box_lens| box_lens.label == label)
+        {
+            let cmd = focal_length.chars().next().unwrap();
+            if cmd == '-' {
+                if let Some(existing_idx) = boxes[idx]
+                    .lenses
+                    .iter()
+                    .position(|box_lens| box_lens.label == label)
+                {
+                    boxes[idx].lenses.remove(existing_idx);
+                }
+            } else if cmd == '=' {
+                // handle =
+                boxes[idx].lenses[existing_lens] = Lens {
+                    label: label.to_string(),
+                    focal_length: focal_length[1..].parse().unwrap(),
+                };
+            }
+        } else if focal_length.starts_with('=') {
+            // handle add new
+            boxes[idx].lenses.push(Lens {
+                label: label.to_string(),
+                focal_length: focal_length.strip_prefix('=').unwrap().parse().unwrap(),
+            });
+        } else {
+            // handle -
+        }
+    }
+
+    // calculate score
+
+    boxes
+        .iter()
+        .enumerate()
+        .flat_map(|(box_num, b)| {
+            b.lenses
+                .iter()
+                .enumerate()
+                .map(|(lens_num, lens)| (box_num + 1) * (lens_num + 1) * lens.focal_length)
+                .collect::<Vec<usize>>()
+        })
+        .sum()
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     const INPUT: &str = include_str!("../input/d15");
-//     const EXAMPLE: &str = include_str!("../examples/d15");
-//
-//     // #[test]
-//     // fn d15p1_example_test() {
-//     //     assert_eq!(
-//     //         part1(parse(EXAMPLE.to_string())),
-//     //         "put part 1 example answer here"
-//     //     );
-//     // }
-//     //
-//     // #[test]
-//     // fn d15p1_input_test() {
-//     //     assert_eq!(
-//     //         part1(parse(INPUT.to_string())),
-//     //         "put part 1 final answer here"
-//     //     );
-//     // }
-//     //
-//     // #[test]
-//     // fn d15p2_example_test() {
-//     //     assert_eq!(
-//     //         part2(parse(EXAMPLE.to_string())),
-//     //         "put part 2 example answer here"
-//     //     );
-//     // }
-//     //
-//     // #[test]
-//     // fn d15p2_input_test() {
-//     //     assert_eq!(
-//     //         part2(parse(INPUT.to_string())),
-//     //         "put part 2 final answer here"
-//     //     );
-//     // }
-// }
+fn hash<S: AsRef<str>>(s: S) -> usize {
+    s.as_ref().chars().fold(0, |sum, c| {
+        (sum as u8).wrapping_add(c as u8).wrapping_mul(17) as usize
+    })
+}
+
+#[derive(Debug, Default, Clone)]
+struct Box {
+    lenses: Vec<Lens>,
+}
+
+#[derive(Debug, Clone)]
+struct Lens {
+    label: String,
+    focal_length: usize,
+}
+
+mod tests {
+    use super::*;
+
+    const INPUT: &str = include_str!("../input/d15");
+    const EXAMPLE: &str = include_str!("../examples/d15");
+
+    #[test]
+    fn d15p1_example_test() {
+        assert_eq!(part1(parse(EXAMPLE.to_string())), 1320);
+    }
+
+    #[test]
+    fn d15p1_input_test() {
+        assert_eq!(part1(parse(INPUT.to_string())), 505459);
+    }
+
+    #[test]
+    fn d15p2_example_test() {
+        assert_eq!(part2(parse(EXAMPLE.to_string())), 145);
+    }
+
+    #[test]
+    fn d15p2_input_test() {
+        assert_eq!(part2(parse(INPUT.to_string())), 228508);
+    }
+}
